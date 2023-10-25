f792bcc8f32ef7ff023ee43b72324aacd8d3ef9d
FanniVarhelyi <fbv5@georgetown.edu>
2023-10-25 15:12:06 -0400

    pie

--- dashboard.py
+++ dashboard.py
@@ -377,8 +377,8 @@ fig.update_layout(
 )
 
 # Add year annotations
-fig.add_annotation(x=0.20, y=0.5, text="2020", font=dict(size=18, color='black'), showarrow=False)
-fig.add_annotation(x=0.81, y=0.5, text=f"{filtered_data['year'].max()}", font=dict(size=18, color='black'), showarrow=False)
+fig.add_annotation(x=0.22, y=0.5, text="2020", font=dict(size=18, color='black'), showarrow=False)
+fig.add_annotation(x=0.82, y=0.5, text=f"{filtered_data['year'].max()}", font=dict(size=18, color='black'), showarrow=False)
 
 st.plotly_chart(fig, use_container_width=True)
 
