0fa794c53b2b5ccd027d29564f496dfaef0d1054
FanniVarhelyi <fbv5@georgetown.edu>
2023-10-25 14:54:05 -0400

    pies

--- dashboard.py
+++ dashboard.py
@@ -355,6 +355,7 @@ fig_max_year = px.pie(
         'Respondents using technology' : '#0C4E6F',
         'Respondents not using technology' : '#13C2FF'
     },
+    title='Test',
     hole=0.6
 )
 
@@ -377,8 +378,8 @@ fig.update_layout(
 )
 
 # Add year annotations
-fig.add_annotation(x=0.20, y=0.5, text="Year 2020", font=dict(size=16, color='black'), showarrow=False)
-fig.add_annotation(x=0.80, y=0.5, text=f"Year {filtered_data['year'].max()}", font=dict(size=16, color='black'), showarrow=False)
+fig.add_annotation(x=0.15, y=0.5, text="Year 2020", font=dict(size=16, color='black'), showarrow=False)
+fig.add_annotation(x=0.75, y=0.5, text=f"Year {filtered_data['year'].max()}", font=dict(size=16, color='black'), showarrow=False)
 
 st.plotly_chart(fig, use_container_width=True)
 
